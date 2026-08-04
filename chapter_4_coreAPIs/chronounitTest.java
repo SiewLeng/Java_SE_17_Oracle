d158973198b1d61d020c7f95b00d7a48a6501fca
SiewLeng <siewleng.lim@gmail.com>
2026-08-04 10:32:36 +0800

    chronunitTest

---
+++ chapter_4_coreAPIs/chronounitTest.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class chronounitTest {
+    public static void test() {
+        var localTime1 = LocalTime.of(5, 15);
+        var localTime2 = LocalTime.of(6, 30);
+        System.out.println(ChronoUnit.HOURS.between(localTime1, localTime2));  
+        System.out.println(ChronoUnit.MINUTES.between(localTime1, localTime2));  
+
+        var localDate1 = LocalDate.of(2025, Month.MAY, 15);
+        var localDate2 = LocalDate.of(2026, Month.MAY, 15);
+        System.out.println(ChronoUnit.YEARS.between(localDate1, localDate2));
+        System.out.println(ChronoUnit.MONTHS.between(localDate1, localDate2));
+        System.out.println(ChronoUnit.DAYS.between(localDate1, localDate2));
+    }
+
+    public static void main(String[] args) {
+        test(); 
+    }
+
+}
