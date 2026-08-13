21c805bbce1fb76c06f4be5325e3fd4376cae43e
SiewLeng <siewleng.lim@gmail.com>
2026-08-13 19:26:24 +0800

    26

---
+++ chapter_6_classDesign/q_26/correct_one/Whale.java
@@ -0,0 +1,26 @@
+package q_26.correct_one;
+
+public abstract class Whale {
+    public abstract void dive();
+    
+    public abstract void dive(int... depth);
+    
+    public static void main(String[] args) {
+        Whale whale = new Orca();
+        whale.dive(3);
+    }
+
+}
+
+class Orca extends Whale {
+    static public int MAX = 3;
+
+    public void dive() {
+       System.out.println("Orca diving");
+    }
+
+    public void dive(int... depth) {
+        System.out.println("Orca diving deeper "+MAX);
+    } 
+
+}
