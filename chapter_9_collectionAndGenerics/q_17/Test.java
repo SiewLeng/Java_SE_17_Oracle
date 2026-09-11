bd38456f292be099e2d3d1a455bcbb6ed1ee6550
SiewLeng <siewleng.lim@gmail.com>
2026-09-11 14:23:23 +0800

    q_17

---
+++ chapter_9_collectionAndGenerics/q_17/Test.java
@@ -0,0 +1,28 @@
+package q_17;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Test {
+    public static void run() {
+        var map = Map.of(1, 2, 3, 6);
+        var list = List.copyOf(map.entrySet());
+
+        List<Integer> one = List.of(8, 16, 2);
+        var copy = List.copyOf(one);
+        var copyOfCopy = List.copyOf(copy);
+        var thirdCopy = new ArrayList<>(copyOfCopy);
+
+        list.replaceAll(x -> x * 2);
+        one.replaceAll(x -> x * 2);
+        copy.replaceAll(x -> x * 2);
+        thirdCopy.replaceAll(x -> x * 2);
+
+        System.out.println(thirdCopy);
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
