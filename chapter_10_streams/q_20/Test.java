0ce821c044b8617554d2b6162218f195a2cf7395
SiewLeng <siewleng.lim@gmail.com>
2026-09-25 12:37:37 +0800

    q_20

--- chapter_10_streams/q_20/Test.java
+++ chapter_10_streams/q_20/Test.java
@@ -4,29 +4,7 @@ import java.util.Optional;
 
 public class Test {
 
-    public static void run1() {
-        Optional<String> opt = Optional.of("1");
-
-        var opt1 = opt.get();
-        System.out.println(opt1);
-
-        var opt2 = opt.orElse("");
-        System.out.println(opt2);
-
-
-        var opt3 = opt.orElseGet(() -> "");
-        System.out.println(opt3);
-        
-        var opt4 = opt.orElseThrow();
-        System.out.println(opt4);
-
-        var opt5 = opt.orElseThrow(RuntimeException::new);
-        System.out.println(opt5);
-
-    }
-
-   public static void run2() {
-        Optional<String> opt = Optional.empty();
+   public static void run(Optional<String> opt) {
 
         try {
             String opt1 = opt.get();
@@ -58,9 +36,9 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        run1();
+        run(Optional.of("1"));
         System.out.println("*********************");
-        run2();
+        run(Optional.empty());
     }
     
 }
